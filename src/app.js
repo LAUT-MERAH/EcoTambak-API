ea123a68e4774180a13c63f13726e6f64b79c7ce
Chrystalio <chrystalio.k@outlook.com>
2024-11-21 19:39:04 +0700

    Refactor app.js ensyre server starts only after successful DB connection

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const connection = require('./config/db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,12 +9,21 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'API is up and running! 🚀 Welcome To EcoTambak API'
-  });
+connection.connect((err) => {
+    if (err) {
+        console.error('Database connection failed:', err.stack);
+        process.exit(1);
+    } else {
+        console.log('Connected to MySQL database');
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.get('/', (req, res) => {
+    res.json({
+        message: 'API is up and running! 🚀 Welcome To EcoTambak API'
+    });
 });
