e048f6aca47864383c38d7b141368c87de6665df
Chrystalio <chrystalio.k@outlook.com>
2024-12-12 21:21:16 +0700

    Add GET endpoint for retrieving user enrollments with token verification

--- src/routes/enrollmentRoutes.js
+++ src/routes/enrollmentRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const enrollmentController = require('../controllers/enrollmentController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
+router.get('/', verifyToken, enrollmentController.getEnrollments);
 router.post('/', verifyToken, enrollmentController.enrollInModule);
 
 module.exports = router;
