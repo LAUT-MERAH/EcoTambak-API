900948dc36da796cc69150b7f6124913543e8a25
Chrystalio <chrystalio.k@outlook.com>
2024-12-12 16:55:49 +0700

    Add getLessons function to retrieve lessons by module ULID

--- src/controllers/lessonController.js
+++ src/controllers/lessonController.js
@@ -1,6 +1,37 @@
 const db = require('../config/db');
 const { ulid } = require('ulid');
 
+exports.getLessons = async (req, res) => {
+    try {
+        const { moduleUlid } = req.params;
+
+        if (!moduleUlid) {
+            return res.status(400).json({ error: 'Module ULID is required!' });
+        }
+
+        const [module] = await db.promise().query(
+            'SELECT * FROM modules WHERE ulid = ?',
+            [moduleUlid]
+        );
+        if (module.length === 0) {
+            return res.status(404).json({ error: 'Module not found!' });
+        }
+
+        const [lessons] = await db.promise().query(
+            'SELECT ulid, title, video_url, thumbnail_url, created_at FROM lessons WHERE module_id = ?',
+            [module[0].id]
+        );
+
+        res.status(200).json({
+            status: 'success',
+            data: lessons
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error!' });
+    }
+};
+
 exports.addLesson = async (req, res) => {
     try {
         const userId = req.user.id;
