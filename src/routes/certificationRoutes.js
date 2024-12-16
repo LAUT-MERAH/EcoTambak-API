3fe84f7de52dc91171a70c7fc5b49a5efb72f959
Chrystalio <chrystalio.k@outlook.com>
2024-12-16 20:04:55 +0700

    Add route to fetch all certificates with token verification

--- src/routes/certificationRoutes.js
+++ src/routes/certificationRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const certificationController = require('../controllers/certificationController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
+router.get('/', verifyToken, certificationController.getAllCertificates);
 router.post('/:enrollmentId/generate', verifyToken, certificationController.generateCertificate);
 
 module.exports = router;
