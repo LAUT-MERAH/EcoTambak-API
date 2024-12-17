72f878a34eafa88864c82bbdd4520bbccb1e6a02
Chrystalio <chrystalio.k@outlook.com>
2024-12-18 01:12:32 +0700

    Add finish redirect route to transaction routes

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -4,6 +4,7 @@ const transactionController = require('../controllers/transactionController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
 router.post('/notifications', transactionController.handleNotification);
+router.get('/finish', transactionController.finishRedirect);
 router.post('/:moduleUlid', verifyToken, transactionController.createTransaction);
 
 module.exports = router;
