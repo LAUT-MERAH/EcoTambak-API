c4cae67e73594c911db0c1b555d511d211d419f6
Chrystalio <chrystalio.k@outlook.com>
2024-12-18 01:53:40 +0700

    Remove unused route for generating certificates in certificationRoutes

--- src/routes/certificationRoutes.js
+++ src/routes/certificationRoutes.js
@@ -4,6 +4,5 @@ const certificationController = require('../controllers/certificationController'
 const { verifyToken } = require('../middleware/authMiddleware');
 
 router.get('/', verifyToken, certificationController.getAllCertificates);
-router.post('/:enrollmentId/generate', verifyToken, certificationController.generateCertificate);
 
 module.exports = router;
