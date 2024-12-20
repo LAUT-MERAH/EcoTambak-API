a111c75c26561dbee7f0970771054f0008db6e09
Chrystalio <chrystalio.k@outlook.com>
2024-12-21 01:37:04 +0700

    Reorder route definitions for module details retrieval

--- src/routes/moduleRoutes.js
+++ src/routes/moduleRoutes.js
@@ -5,8 +5,8 @@ const { verifyToken } = require('../middleware/authMiddleware');
 const { isInstructor } = require('../middleware/instructorMiddleware');
 
 router.get('/', moduleController.getAllModules);
-router.get('/:moduleUlid', verifyToken, moduleController.getModuleDetails);
 router.get('/my-modules', verifyToken, isInstructor, moduleController.getInstructorModules);
+router.get('/:moduleUlid', verifyToken, moduleController.getModuleDetails);
 router.post('/', verifyToken, isInstructor, moduleController.createModule);
 router.patch('/:moduleUlid', verifyToken, isInstructor, moduleController.updateModule);
 
